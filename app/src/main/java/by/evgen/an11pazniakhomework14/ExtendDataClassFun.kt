380fae7a11e7b3dfe7faf046c75092ca85f8c9cf
Yauheni <yauhenij@gmail.com>
2021-12-03 17:53:10 +0300

    Remove class-extend. Added file with extends function

---
+++ app/src/main/java/by/evgen/an11pazniakhomework14/ExtendDataClassFun.kt
@@ -0,0 +1,5 @@
+package by.evgen.an11pazniakhomework14
+
+fun CatDataClass.countFields(): Int {
+    return this.catAge + this.tailLength + this.countPaws
+}
--- app/src/main/java/by/evgen/an11pazniakhomework14/ExtendsDataClassFun.kt
+++
@@ -1,7 +0,0 @@
-package by.evgen.an11pazniakhomework14
-
-class ExtendsDataClassFun { //Создать файл kotlin в котором необходимо написать функцию расширения для data класса считающую и возвращающую сумму полей data class
-    fun CatDataClass.countFields(): Int {
-        return this.catAge + this.tailLength + this.countPaws
-    }
-}
